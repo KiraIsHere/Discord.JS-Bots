4ae5b99d27379f3237423f8a7e5aeb86cd073905
ShayBox <shayneehartford@gmail.com>
2017-10-14 01:01:36 -0400

    Fixed calls command

--- src/Bots/Cleverbot/Commands/calls.js
+++ src/Bots/Cleverbot/Commands/calls.js
@@ -17,7 +17,7 @@ class Command extends Commands {
 
 	run(client, message) {
 		client.database.find({}).then(data => {
-			client.send(message, `${data[0].USED_API_CALLS} / ${data[1].TOTAL_API_CALLS}\n`);
+			client.send(message, `${data[0].USED_API_CALLS} / ${data[0].TOTAL_API_CALLS}\n`);
 		});
 	}
 }
