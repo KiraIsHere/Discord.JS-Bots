e36fc96e523630542f6bc7d5358466506dbe4883
ShayBox <shayneehartford@gmail.com>
2017-10-24 13:18:07 -0400

    Added ip command Removed delete command Added aliases to exit command

--- src/Bots/Shay/Commands/ip.js
+++ src/Bots/Shay/Commands/ip.js
@@ -1,11 +1,12 @@
-const Commands = require(`../Structures/Commands`);
+const Commands = require(`../../../__Global/Structures/Commands`);
 const { basename } = require(`path`);
+const { get } = require(`snekfetch`);
 
 class Command extends Commands {
 	constructor(client) {
 		super(client, {
 			enabled: true,
-			show: false,
+			show: true,
 			cooldown: false,
 			cooldownAmount: 1,
 			cooldownTime: 3,
@@ -13,15 +14,16 @@ class Command extends Commands {
 			limitAmount: 3,
 			limitTime: 86400,
 			name: basename(__filename, `.js`),
-			description: `Runs process.exit()`,
-			usage: `Exit`,
-			aliases: [`process.exit()`, `exit`, `restart`]
+			description: `Gives the server's ip address`,
+			usage: `IP`,
+			aliases: []
 		});
 	}
 
 	run(client, message) {
-		if (!client.ownerIDs.includes(message.author.id)) return client.send(message, `Sorry, you do not have permission for this command`);
-		process.exit();
+		if (process.env.LOCAL_TESTING) return false;
+
+		get(`https://api.ipify.org/`).then(r => client.send(message, r.text));
 		return true;
 	}
 }
