ad4241a2016b669c4362c859332e3588dc0f2856
ShayBox <shayneehartford@gmail.com>
2017-10-25 06:57:47 -0400

    Added Neko commands

---
+++ src/Bots/Shay/Commands/neko.js
@@ -0,0 +1,31 @@
+const Commands = require(`../../../__Global/Structures/Commands`);
+const { get } = require(`snekfetch`);
+const { basename } = require(`path`);
+
+class Command extends Commands {
+	constructor(client) {
+		super(client, {
+			enabled: true,
+			show: true,
+			cooldown: false,
+			cooldownAmount: 1,
+			cooldownTime: 3,
+			limit: false,
+			limitAmount: 3,
+			limitTime: 86400,
+			name: basename(__filename, `.js`),
+			description: `Random neko picture`,
+			usage: ``,
+			aliases: []
+		});
+	}
+
+	run(client, message) {
+		get(`http://nekos.life/api/neko`).then(data => {
+			client.send(message, `test`, { files: [data.body.neko] });
+		});
+		return true;
+	}
+}
+
+module.exports = Command;
