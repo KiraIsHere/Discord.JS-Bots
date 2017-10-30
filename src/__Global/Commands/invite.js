0b66df4731011a88f12761305f9495719bf23da7
ShayBox <shayneehartford@gmail.com>
2017-10-29 21:11:35 -0400

    Updated Invite command to add dbots invite link

--- src/__Global/Commands/invite.js
+++ src/__Global/Commands/invite.js
@@ -21,18 +21,22 @@ class Command extends Commands {
 	}
 
 	run(client, message) {
-		let embed = new MessageEmbed();
+		let embed = new MessageEmbed()
+			.setColor(0x00FFFF);
 
 		if (client.user.bot) {
 			embed
-				.setTitle(`Invite Link`)
-				.setURL(`https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8`)
-				.setColor(0x00FFFF)
-				.setFooter(`Note: I may be a private bot`);
+				.setTitle(`Invite Link`);
+			if (client.user.id === `361796552165031936`) {
+				embed.setURL(`https://bots.discord.pw/bots/${client.user.id}`);
+			} else {
+				embed
+					.setURL(`https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8`)
+					.setFooter(`Note: I may be a private bot`);
+			}
 		} else {
 			embed
-				.setTitle(`I'm a user account, I can't be invited`)
-				.setColor(0x00FFFF);
+				.setTitle(`I'm a user account, I can't be invited`);
 		}
 		client.send(message, { embed });
 		return true;
