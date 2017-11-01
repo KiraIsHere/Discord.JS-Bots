5c9f16e03a836e4647572db035feed994f64846f
ShayBox <shayneehartford@gmail.com>
2017-10-31 22:09:34 -0400

    Added live activity updating

--- src/__Global/Events/guildDelete.js
+++ src/__Global/Events/guildDelete.js
@@ -5,6 +5,8 @@ class Event extends Events {
 	run(client, guild) {
 		if (process.env.LOCAL) return false;
 
+		if (client.user.bot) client.user.setActivity(`${client.botPrefix}help | ${client.guilds.size} ${client.guilds.size > 1 ? `Guilds` : `Guild`} | By Shayne Hartford (ShayBox)`);
+
 		const embed = new MessageEmbed()
 			.setAuthor(guild.name, guild.iconURL())
 			.setColor(0xFF0000)
