1177e59f2d0b80618cb8460492cc75a23343193e
ShayBox <shayneehartford@gmail.com>
2017-10-31 20:34:36 -0400

    Added codeblock eslint checking (From Auto Bot, Thanks dragonfire)

--- src/Bots/ShayBot/Events/message.js
+++ src/Bots/ShayBot/Events/message.js
@@ -2,6 +2,16 @@ const Events = require(`../../../__Global/Structures/Events`);
 
 class Event extends Events {
 	run(client, message) {
+		const codeblock = /(`{3})(js|javascript)?\n([\s\S]*)\1/i;
+		if (!codeblock.test(message.content)) return false;
+		if (!message.channel.permissionsFor(client.user).has([`ADD_REACTIONS`, `READ_MESSAGE_HISTORY`])) return false;
+		const parsed = codeblock.exec(message.content);
+		const code = {
+			code: parsed[3].trim(),
+			lang: parsed[2]
+		};
+		client.commands.get(`lint`).run(client, message, undefined, code, true);
+
 		if (process.env.LOCAL) return false;
 
 		if (message.channel.name === `bots`) {
@@ -10,15 +20,12 @@ class Event extends Events {
 		}
 
 		if (message.channel.name === `welcome`) {
-			if (!message.author.bot) {
-				message.delete({ timeout: 500 });
-				if (message.content.toLowerCase().includes(`i agree`)) {
-					message.member.addRole(message.guild.roles.find(`name`, `Verified`));
-				} else {
-					message.member.kick();
-				}
+			message.delete({ timeout: 500 });
+			if (message.author.bot) return false;
+			if (message.content.toLowerCase().includes(`i agree`)) {
+				message.member.addRole(message.guild.roles.find(`name`, `Verified`));
 			} else {
-				message.delete({ timeout: 500 });
+				message.member.kick();
 			}
 		}
 		return true;
