15b6ecf8c5149d2a8bda8fc058e03e0089d3e5b4
ShayBox <shayneehartford@gmail.com>
2017-11-06 03:10:45 -0500

    Removed marv command deletion

--- src/Bots/ShayBot/Events/message.js
+++ src/Bots/ShayBot/Events/message.js
@@ -6,11 +6,6 @@ class Event extends Events {
 
 		if (process.env.LOCAL) return false;
 
-		if (message.channel.name === `bots`) {
-			if (message.content.startsWith(`-`)) message.delete({ timeout: 1000 * 300 }).catch(() => null);
-			if (message.author.id === `234395307759108106`) message.delete({ timeout: 1000 * 300 }).catch(() => null);
-		}
-
 		if (message.channel.name === `welcome`) {
 			message.delete({ timeout: 500 });
 			if (message.author.bot) return false;
