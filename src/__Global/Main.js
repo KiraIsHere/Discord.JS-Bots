b90c0e256736ff162a44eef7e532f09c9d127954
ShayBox <shayneehartford@gmail.com>
2017-11-17 17:49:41 -0500

    Moved client options to seperate json files per bot to save ram Removed engines section from package.json to keep it LTS

--- src/__Global/Main.js
+++ src/__Global/Main.js
@@ -1,49 +1,10 @@
-const Client = require(`../__Global/Structures/Client`);
-const Util = require(`../__Global/Structures/Util`);
-const Loader = new Util;
+const Client = require(`./Structures/Client`);
+const Util = require(`./Structures/Util`);
+const { resolve, join } = require(`path`);
+const Options = require(join(resolve(`.`), `./Options`));
 
-const client = new Client({
-	messageCacheMaxSize: 100,
-	messageCacheLifetime: 600,
-	messageSweepInterval: 10,
-	disabledEvents: [
-		`CHANNEL_CREATE`,
-		`CHANNEL_DELETE`,
-		`CHANNEL_PINS_UPDATE`,
-		`CHANNEL_UPDATE`,
-		// `GUILD_BAN_ADD`,
-		// `GUILD_BAN_REMOVE`,
-		// `GUILD_CREATE`,
-		// `GUILD_DELETE`,
-		// `GUILD_MEMBERS_CHUNK`,
-		// `GUILD_MEMBER_ADD`,
-		// `GUILD_MEMBER_REMOVE`,
-		`GUILD_MEMBER_UPDATE`,
-		`GUILD_ROLE_CREATE`,
-		`GUILD_ROLE_DELETE`,
-		`GUILD_ROLE_UPDATE`,
-		`GUILD_SYNC`,
-		`GUILD_UPDATE`,
-		// `MESSAGE_CREATE`,
-		`MESSAGE_DELETE_BULK`,
-		`MESSAGE_DELETE`,
-		// `MESSAGE_REACTION_ADD`,
-		`MESSAGE_REACTION_REMOVE_ALL`,
-		`MESSAGE_REACTION_REMOVE`,
-		// `MESSAGE_UPDATE`,
-		`PRESENCE_UPDATE`,
-		// `READY`,
-		`RELATIONSHIP_ADD`,
-		`RELATIONSHIP_REMOVE`,
-		// `RESUMED`,
-		`TYPING_START`,
-		`USER_NOTE_UPDATE`,
-		`USER_SETTINGS_UPDATE`,
-		`USER_UPDATE`
-		// `VOICE_SERVER_UPDATE`,
-		// `VOICE_STATE_UPDATE`
-	]
-});
+const client = new Client(Options);
+const Loader = new Util;
 
 Loader.init(client).then(() => {
 	client.login(process.env[client.botName]).catch(error => { throw error; });
