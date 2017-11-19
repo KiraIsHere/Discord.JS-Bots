a7f386963bcab407d5874eab9834c5f7e5d19d8c
ShayBox <shayneehartford@gmail.com>
2017-11-19 11:03:59 -0500

    Fixed bug

--- src/Bots/ShayBot/Events/message.js
+++ src/Bots/ShayBot/Events/message.js
@@ -6,7 +6,7 @@ class Event extends Events {
 
 		if (process.env.LOCAL) return false;
 
-		if (!message.channel.name === `welcome`) return false;
+		if (message.channel.name !== `welcome`) return false;
 		message.delete({ timeout: 500 });
 		if (message.author.bot) return false;
 		if (!message.content.toLowerCase().includes(`i agree`)) return message.member.kick();
