7460843edf580c85d01aec33e68338a42c528ca2
ShayBox <shayneehartford@gmail.com>
2017-11-24 00:02:03 -0500

    Added check to only do it if its more than 0

--- src/Bots/ShayBot/Events/ready.js
+++ src/Bots/ShayBot/Events/ready.js
@@ -3,7 +3,9 @@ const Events = require(`../../../__Global/Structures/Events`);
 class Event extends Events {
 	run(client) {
 		if (process.env.LOCAL) return true;
-		client.guilds.get(client.servers.MAIN).pruneMembers({ days: 1 });
+		client.guilds.get(client.servers.MAIN).pruneMembers({ days: 1, dry: true }).then(number => {
+			if (number > 0) client.guilds.get(client.servers.MAIN).pruneMembers({ days: 1 });
+		});
 		return true;
 	}
 }
