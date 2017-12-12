01dad71d040a803c3ed87598c90e72aafc160a62
ShayBox <shayneehartford@gmail.com>
2017-12-12 14:35:07 -0500

    Removed DM bot

--- src/Bots/ShayBot/Events/ready.js
+++ src/Bots/ShayBot/Events/ready.js
@@ -1,27 +1,11 @@
 const Events = require(`../../../__Global/Structures/Events`);
-const { get } = require(`snekfetch`);
 
 class Event extends Events {
 	run(client) {
 		if (process.env.LOCAL) return true;
 		const guild = client.guilds.get(client.servers.MAIN);
 		guild.pruneMembers({ days: 1, dry: true }).then(number => number > 0 ? guild.pruneMembers({ days: 1 }) : false);
-
-		setInterval(async () => {
-			client.users.get(`90323448357736448`).send(
-				`BTC: ${await this.getCoin(`BTC`)} USD\n` +
-				`ETH: ${await this.getCoin(`ETH`)} USD\n` +
-				`LTC: ${await this.getCoin(`LTC`)} USD\n` +
-				`MONA: ${await this.getCoin(`MONA`)} USD\n` +
-				`ETN: ${await this.getCoin(`ETN`)} USD\n` +
-				`Theese are the most accurate values of worth you can calulate.`
-			);
-		}, 1000 * 10 * 30);
 		return true;
-	}
-
-	getCoin(coin) {
-		return get(`https://min-api.cryptocompare.com/data/price?fsym=${coin}&tsyms=USD`).then(data => data.body.USD);
 	}
 }
 
