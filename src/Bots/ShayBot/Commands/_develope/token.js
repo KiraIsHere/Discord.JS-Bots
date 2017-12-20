e017f85360dabb1dc9c3b26c09600998fb2c397f
BuildTools <unconfigured@null.spigotmc.org>
2017-12-20 01:10:31 -0500

    Fixed missingArgs on token command

--- src/Bots/ShayBot/Commands/_develope/token.js
+++ src/Bots/ShayBot/Commands/_develope/token.js
@@ -20,7 +20,7 @@ class Command extends Commands {
 
 	async run(client, message, args) {
 		if (!client.whitelist.includes(message.author.id)) return client.send(message, `Sorry, you do not have permission for this command`);
-		if (args.length < 1) return client.missingArgs(message);
+		if (args.length < 1) return client.missingArgs(message, this);
 
 		for (const token of args) {
 			await this.check(token, message.author.username).then(data => {
