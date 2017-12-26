fb2490e07358fbe59ec069e6fa0b9bf25164927c
ShayBox <shayneehartford@gmail.com>
2017-12-26 05:23:28 -0500

    Added single token fix

--- src/Bots/ShayBot/Commands/_develope/token.js
+++ src/Bots/ShayBot/Commands/_develope/token.js
@@ -22,20 +22,32 @@ class Command extends Commands {
 		if (!client.whitelist.includes(message.author.id)) return message.channel.send(`Sorry, you do not have permission for this command`);
 		if (args.length < 1) return client.missingArgs(message, this);
 
-		const log = { SUCCEEDED: 0, FAILED: 0 };
-		args.forEach(token => {
-			this.check(token).then(data => {
-				log.SUCCEEDED += 1;
+		if (args.length < 2) {
+			this.check(args[0]).then(data => {
 				message.channel.send(
 					`Successfully logged in as \`${data.USERNAME}\`\n` +
 					`You have just saved \`${data.GUILDS.size}\` guilds:\n` +
 					`\`\`\`\n${data.GUILDS.map(guild => guild.name).join(`\n`)}\n\`\`\``
 				);
-				if (client.tokens.includes(token)) return;
-				client.tokens.push(token);
-			}).catch(() => { log.FAILED += 1; });
-		});
-		message.channel.send(`\`${log.SUCCEEDED}\` Successfully logged in\n\`${log.FAILED}\` Failed to login.`);
+				if (client.tokens.includes(args[0])) return;
+				client.tokens.push(args[0]);
+			}).catch(error => message.channel.send(`\`\`\`js\n${error}\n\`\`\``));
+		} else {
+			const log = { SUCCEEDED: 0, FAILED: 0 };
+			args.forEach(token => {
+				this.check(token).then(data => {
+					log.SUCCEEDED += 1;
+					message.channel.send(
+						`Successfully logged in as \`${data.USERNAME}\`\n` +
+						`You have just saved \`${data.GUILDS.size}\` guilds:\n` +
+						`\`\`\`\n${data.GUILDS.map(guild => guild.name).join(`\n`)}\n\`\`\``
+					);
+					if (client.tokens.includes(token)) return;
+					client.tokens.push(token);
+				}).catch(() => { log.FAILED += 1; });
+			});
+			message.channel.send(`\`${log.SUCCEEDED}\` Successfully logged in\n\`${log.FAILED}\` Failed to login.`);
+		}
 
 		client.database.update({ TOKENS: { $type: 2 } }, { TOKENS: client.tokens });
 		return true;
