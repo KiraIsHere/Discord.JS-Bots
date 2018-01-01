d4084eb7774c8c99ecf49e4ba5572ebbe475a151
ShayBox <shayneehartford@gmail.com>
2018-01-01 16:17:49 -0500

    Fixed offset

--- src/Bots/ShayBot/Commands/utility/clear.js
+++ src/Bots/ShayBot/Commands/utility/clear.js
@@ -21,7 +21,7 @@ class Command extends Commands {
 		if (args.length < 1 || Number.isInteger(args[0]) || args[0] < 1 || args[0] > 100) return client.missingArgs(message, this)
 		if (!message.guild.me.hasPermission(`MANAGE_MESSAGES`) || !message.member.hasPermission(`MANAGE_MESSAGES`)) return message.channel.send(`Missing Permissions "MANAGE_MESSAGES"`)
 
-		message.channel.bulkDelete(args[0], { filterOld: true })
+		message.channel.bulkDelete(args[0] + 1, { filterOld: true })
 		return true
 	}
 }
