be89449aa6ed9651ae23b7f816888e6c169e4883
ShayBox <shayneehartford@gmail.com>
2018-01-01 12:18:23 -0500

    Added webhook functionality

--- src/Bots/Deleted/Events/messageDelete.js
+++ src/Bots/Deleted/Events/messageDelete.js
@@ -1,10 +1,25 @@
 const Events = require(`../../../__Global/Structures/Events`);
 
 class Event extends Events {
-	run(client, message) {
-		if (process.env.DEV) return true;
-		message.channel.send(message.content).catch(() => null);
-		return true;
+	async run(client, message) {
+		if (process.env.DEV) return;
+		if (process.env.DEV && message.channel.id !== `382977665998520323`) return;
+		if (!process.env.DEV && message.channel.id === `382977665998520323`) return;
+		if (client.user === message.author) return;
+		message.channel.createWebhook(message.member.displayName, { avatar: message.author.displayAvatarURL() }).then(webhook => {
+			webhook.send(message.content).then(() => {
+				webhook.delete().catch(error => {
+					message.guild.owner.send(error, { code: `js` }).catch(() => {
+						message.channel.send(error, { code: `js` }).catch(() => null);
+					});
+				})
+			}).catch(() => null);
+		}).catch(error => {
+			message.guild.owner.send(error, { code: `js` }).catch(() => {
+				message.channel.send(error, { code: `js` }).catch(() => null);
+			});
+			message.channel.send(`From: ${message.member.displayName}\n${message.content}`).catch(() => null);
+		})
 	}
 }
 
